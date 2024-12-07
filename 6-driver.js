f21716aae69124849baa12026aca35ee60414f93
Igor Kgm <>
2024-12-07 22:50:05 +0300

    del

--- 6-driver.js
+++
@@ -1,6 +0,0 @@
-const licence = true;
-const age = 18;
-const isDrunk = false;
-const canDrive = licence && age >= 18 && !isDrunk;
-
-console.log(`${canDrive ? 'Может' : 'Не может'} водить автомобиль`);
